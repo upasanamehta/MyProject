529f30b0d46b30c547c37db89904fab799048d4d
Upasana Mehta <umehta@expedia.com>
2016-06-24 19:53:41 +0530

    push

---
+++ src/main/java/expedia/http/Ping.java
@@ -0,0 +1,31 @@
+package expedia.http;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by umehta on 6/23/16.
+ */
+
+@RestController
+public class Ping {
+
+    final static String hello = "hello";
+
+    @RequestMapping(value = "/ping" , method = RequestMethod.GET )
+    public @ResponseBody String ping() throws IOException {
+        final Properties properties = new Properties();
+        properties.load(this.getClass().getResourceAsStream("application.properties"));
+        String projectVersion = properties.getProperty("project.version");
+        return "pong \n Project Version: "+projectVersion;
+
+    }
+
+}
